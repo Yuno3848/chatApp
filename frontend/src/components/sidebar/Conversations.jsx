00788e65476d908d4f5f47786de021ff299ba13c
Yuno3848 <bhandarianurag1234@gmail.com>
2025-06-29 06:49:49 +0530

    added message section UI

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -5,6 +5,10 @@ const Conversations = () => {
   return (
     <div className="py-2 flex flex-col overflow-auto">
       <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
     </div>
   );
 };
