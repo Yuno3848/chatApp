a7c3186eeaed8c5785d82f43420ca35d0bfb0eca
Yuno3848 <bhandarianurag1234@gmail.com>
2025-07-04 19:39:48 +0530

    added login functionality & installed zustand

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,50 @@
+import toast from "react-hot-toast";
+
+const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+  const login = async (email, password) => {
+    const success = handleErrors({
+      email,
+      password,
+    });
+    if (!success) return;
+    console.log(`succcess : ${success}`);
+    setLoading(true);
+    try {
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      setAuthUser(data);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { loading, login };
+};
+const handleErrors = ({ password, email }) => {
+  if (!password || !email) {
+    toast.error("All fields are required!");
+    return false;
+  }
+  if (password !== confirmPassword) {
+    toast.error("Passwords do not match.");
+    return false;
+  }
+  if (password.length < 6) {
+    toast.error("Password must be greater than 6 characters.");
+    return false;
+  }
+  return true;
+};
+export default useLogin;
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { loading, login } = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(email, password);
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-cover bg-center px-4">
       <div className="w-full max-w-md p-10 bg-white/10 border border-white/20 rounded-3xl shadow-2xl backdrop-blur-lg">
@@ -9,15 +19,17 @@ const Login = () => {
           Login to <span className="text-cyan-400">ChatChai</span>
         </h2>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-control mb-6">
             <label className="label">
-              <span className="label-text text-white text-base">Username</span>
+              <span className="label-text text-white text-base">Email</span>
             </label>
             <input
               type="text"
               placeholder="Enter username"
               className="input input-bordered input-md bg-white/30 text-white placeholder-white/70 border-white/30"
+              value={email}
+              onChange={() => setEmail(e.target.value)}
             />
           </div>
 
@@ -29,6 +41,8 @@ const Login = () => {
               type="password"
               placeholder="Enter password"
               className="input input-bordered input-md bg-white/30 text-white placeholder-white/70 border-white/30"
+              value={password}
+              onChange={() => setPassword(e.target.value)}
             />
           </div>
           <Link
@@ -38,8 +52,15 @@ const Login = () => {
             Don't have an account?
           </Link>
 
-          <button className="btn btn-primary btn-md w-full hover:scale-[1.02] transition-transform">
-            Login
+          <button
+            className="btn btn-primary btn-md w-full hover:scale-[1.02] transition-transform"
+            disabled={loading}
+          >
+            {loading ? (
+              <span className="loading loading-spinner"></span>
+            ) : (
+              "Login"
+            )}
           </button>
         </form>
       </div>
